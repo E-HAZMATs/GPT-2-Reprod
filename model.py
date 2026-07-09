6766abdcd95da8c50eabdd9d5917344328d1b3f3
eHAZMATs <alzamarkal@gmail.com>
2026-07-09 10:17:51 +0300

    checkpoint. change hype param names to match gpt2.

---
+++ model.py
@@ -0,0 +1,141 @@
+from dataclasses import dataclass
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+import math
+
+class CausalSelfAttention(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        
+        self.n_embed, self.n_head, block_size= config.n_embed, config.n_head, config.block_size
+
+        # Since we're doing multi-headed attention in one module insead of different modules,
+        # We would give equal parts of the embedding vector to each head. So we need to make sure the're divisible.
+        assert self.n_embed % self.n_head == 0
+
+        # Get qkv in one layer, later we split to get each. More efficient this way.
+        self.c_attn = nn.Linear(self.n_embed, self.n_embed * 3)
+
+        self.c_proj = nn.Linear(self.n_embed, self.n_embed)
+        # This is the stencil/mask for the affinity matrix. Called bias to match gpt2 implementation.
+        # register buffers allow us to add args i nthe module without having them as params (so no grads).
+        self.register_buffer('bias', torch.tril(torch.ones(block_size, block_size)).view(1, 1, block_size, block_size))
+
+    def forward(self, x):
+        B, T, C = x.size()
+
+        qkv = self.c_attn(x)
+        q, k, v = qkv.split(self.n_embed, dim=2)
+        # (B, n_head, T, Head_size)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) 
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+
+        attn = (q @ k.transpose(-1, -2)) * ( 1 / math.sqrt(k.size(-1)))
+        att = att.masked_fill(self.bias[:, :, :T, :T] == 0, float('-inf'))
+        att = F.softmax(att, dim=1)
+        y = att @ v
+        y = y.transpose(1, 2).contiguous().view(B, T, C)
+        y = self.c_proj(y)
+        return y
+
+class MLP(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc = nn.Linear(config.n_embed, config.n_embed * 4)
+        self.gelu = nn.GELU(approximate=True)
+        self.c_proj = nn.Linear(config.n_embed * 4, config.n_embed)
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        return x
+    
+class Block(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.ln1 = nn.LayerNorm(config.n_embed)
+        self.attn = CausalSelfAttention(config)
+        self.ln2 = nn.LayerNorm(config.n_embed)
+        self.mlp = MLP(config)
+
+    def forward(self, x):
+        x = x + self.attn(self.ln1(x))
+        x = x + self.mlp(self.ln2(x))
+        return x
+@dataclass
+class GPTConfig:
+    block_size = 0;
+    n_embed = 0
+    n_block = 0
+    n_head = 0
+    vocab_size = 0
+    batch_size = 32
+
+
+class GPT(nn.Module):
+    
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+
+        self.transformer = nn.ModuleDict(dict(
+            wte = nn.Embedding(config.vocab_size, config.n_embed),
+            wpe = nn.Embedding(config.block_size, config.n_embed),
+            blocks = nn.ModuleList([Block(config) for _ in range(config.n_block)]),
+            ln_f = nn.LayerNorm(config.n_embed),
+        ))
+
+        self.lm_head = nn.Linear(config.n_embed, config.vocab_size, bias=False)
+
+    @classmethod
+    def from_pretrained(cls, model_type):
+        # Loading a pretrained model from hugging face.
+        assert model_type in {'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'}
+        from transformers import GPT2LMHeadModel
+
+        print(f'loading weights from pretrain model {model_type}')
+
+        config_args = {
+            "gpt2": dict(n_layer=12, n_head=12, n_embd=768), # I named n_layer n_block in my config. #FIXME: Also n_embd i named n_embed (must match gpt2 names?)
+            "gpt2-medium": dict(n_layer=24, n_head=16, n_embd=1024),
+            "gpt2-large": dict(n_layer=36, n_head=20, n_embd=1280),
+            "gpt2-xl": dict(n_layer=48, n_head=25, n_embd=1600),
+        }[model_type]
+        config_args['vocab_size'] = 50257
+        config_args['block_size'] = 1024
+
+        config = GPTConfig(**config_args)
+        model = GPT(config)
+        sd = model.state_dict()
+        sd_keys = sd.keys()
+        sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')] # Remove stencil. Not a param.
+
+        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
+        sd_hf = model_hf.state_dict()
+
+        sd_keys_hf = sd_hf.keys()
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.masked_bias') and not k.endswith('.attn.bias')]
+        transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
+
+        assert len(sd_keys_hf) == len(sd_keys), f'states dicts don\'t match.'
+        for k in sd_keys_hf:
+            if any(k.endswith(w) for w in transposed):
+                # Conv1D weights.
+                assert sd_hf[k].shape[::-1] == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k].t())
+            else:
+                assert sd_hf[k].shape == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
+
+        return model
+
+model = GPT.from_pretrained('gpt2')
+print('survived crashing ^_^')
