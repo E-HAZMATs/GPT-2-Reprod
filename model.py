dd35d111b7ebb3305b2aea3e711edda899dedabe
eHAZMATs <alzamarkal@gmail.com>
2026-07-09 10:44:56 +0300

    Model loading from hf works. Had to change names of layers and hypeparams to match GPT2 names.

--- model.py
+++ model.py
@@ -9,16 +9,16 @@ class CausalSelfAttention(nn.Module):
     def __init__(self, config):
         super().__init__()
         
-        self.n_embed, self.n_head, block_size= config.n_embed, config.n_head, config.block_size
+        self.n_embd, self.n_head, block_size= config.n_embd, config.n_head, config.block_size
 
         # Since we're doing multi-headed attention in one module insead of different modules,
         # We would give equal parts of the embedding vector to each head. So we need to make sure the're divisible.
-        assert self.n_embed % self.n_head == 0
+        assert self.n_embd % self.n_head == 0
 
         # Get qkv in one layer, later we split to get each. More efficient this way.
-        self.c_attn = nn.Linear(self.n_embed, self.n_embed * 3)
+        self.c_attn = nn.Linear(self.n_embd, self.n_embd * 3)
 
-        self.c_proj = nn.Linear(self.n_embed, self.n_embed)
+        self.c_proj = nn.Linear(self.n_embd, self.n_embd)
         # This is the stencil/mask for the affinity matrix. Called bias to match gpt2 implementation.
         # register buffers allow us to add args i nthe module without having them as params (so no grads).
         self.register_buffer('bias', torch.tril(torch.ones(block_size, block_size)).view(1, 1, block_size, block_size))
@@ -27,7 +27,7 @@ class CausalSelfAttention(nn.Module):
         B, T, C = x.size()
 
         qkv = self.c_attn(x)
-        q, k, v = qkv.split(self.n_embed, dim=2)
+        q, k, v = qkv.split(self.n_embd, dim=2)
         # (B, n_head, T, Head_size)
         q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) 
         k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
@@ -45,9 +45,9 @@ class MLP(nn.Module):
 
     def __init__(self, config):
         super().__init__()
-        self.c_fc = nn.Linear(config.n_embed, config.n_embed * 4)
+        self.c_fc = nn.Linear(config.n_embd, config.n_embd * 4)
         self.gelu = nn.GELU(approximate=True)
-        self.c_proj = nn.Linear(config.n_embed * 4, config.n_embed)
+        self.c_proj = nn.Linear(config.n_embd * 4, config.n_embd)
 
     def forward(self, x):
         x = self.c_fc(x)
@@ -59,9 +59,9 @@ class Block(nn.Module):
 
     def __init__(self, config):
         super().__init__()
-        self.ln1 = nn.LayerNorm(config.n_embed)
+        self.ln_1 = nn.LayerNorm(config.n_embd)
         self.attn = CausalSelfAttention(config)
-        self.ln2 = nn.LayerNorm(config.n_embed)
+        self.ln_2 = nn.LayerNorm(config.n_embd)
         self.mlp = MLP(config)
 
     def forward(self, x):
@@ -70,12 +70,12 @@ class Block(nn.Module):
         return x
 @dataclass
 class GPTConfig:
-    block_size = 0;
-    n_embed = 0
-    n_block = 0
-    n_head = 0
-    vocab_size = 0
-    batch_size = 32
+    block_size: int= 1024
+    n_embd: int = 768
+    n_layer: int = 12
+    n_head: int = 12
+    vocab_size: int = 50257
+    batch_size: int = 32
 
 
 class GPT(nn.Module):
@@ -85,13 +85,13 @@ class GPT(nn.Module):
         self.config = config
 
         self.transformer = nn.ModuleDict(dict(
-            wte = nn.Embedding(config.vocab_size, config.n_embed),
-            wpe = nn.Embedding(config.block_size, config.n_embed),
-            blocks = nn.ModuleList([Block(config) for _ in range(config.n_block)]),
-            ln_f = nn.LayerNorm(config.n_embed),
+            wte = nn.Embedding(config.vocab_size, config.n_embd),
+            wpe = nn.Embedding(config.block_size, config.n_embd),
+            h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
+            ln_f = nn.LayerNorm(config.n_embd),
         ))
 
-        self.lm_head = nn.Linear(config.n_embed, config.vocab_size, bias=False)
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
 
     @classmethod
     def from_pretrained(cls, model_type):
@@ -102,7 +102,7 @@ class GPT(nn.Module):
         print(f'loading weights from pretrain model {model_type}')
 
         config_args = {
-            "gpt2": dict(n_layer=12, n_head=12, n_embd=768), # I named n_layer n_block in my config. #FIXME: Also n_embd i named n_embed (must match gpt2 names?)
+            "gpt2": dict(n_layer=12, n_head=12, n_embd=768),
             "gpt2-medium": dict(n_layer=24, n_head=16, n_embd=1024),
             "gpt2-large": dict(n_layer=36, n_head=20, n_embd=1280),
             "gpt2-xl": dict(n_layer=48, n_head=25, n_embd=1600),
