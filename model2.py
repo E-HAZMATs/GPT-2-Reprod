897d457675b799b21ce14023d61a61136b6c4a03
eHAZMATs <alzamarkal@gmail.com>
2026-07-15 16:27:38 +0300

    checkpoint. tryna implement `topk` selection.

--- model2.py
+++ model2.py
@@ -34,7 +34,7 @@ class SelfAttention(nn.Module):
         # rough steps: get QKV > matmul q with k > get mask > apply mask > softmax 
         # > matmul softmax result (wei) with V -> project.
         B, T, C = x.size() 
-        assert C <= self.config.embedding_len, "wtf?" 
+        assert C <= self.config.embedding_len, "huh?" 
         qkv = self.qkv_lin(x) # B T EMBED_LEN*3
         q, k, v = qkv.split(self.config.embedding_len, -1) # split over emb dim
         
@@ -91,38 +91,78 @@ class Block(nn.Module):
         output: (B, T, C)
         '''
         x = self.ln_1(x)
-        x = x + self.attn(x)
+        x = x + self.attn(x) # attn is residual block output added to residual pathway
         x = self.ln_2(x)
-        x = x + self.feed_fw(x)
+        x = x + self.feed_fw(x) # feed_fw is residual block output added to residual pathway
         return x
 
 class GPT(nn.Module):
-
+    
     def __init__(self, config:GPTConfig):
+        '''
+        TODO: Apply weight tying between output layer and embedding layer. Reduces params lots without ruining performance.
+        '''
         super().__init__()
+        self.config = config
+        self.top_picks = 5 # Arg for torch.topk
         self.transformer = nn.ModuleDict(dict(
             embedding_table = nn.Embedding(config.vocab_size, config.embedding_len),
             positional_embedding = nn.Embedding(config.seq_len, config.embedding_len),
             blocks = nn.ModuleList([Block(config) for _ in range(config.n_blocks)]), 
-            ln = nn.LayerNorm()
+            ln = nn.LayerNorm(config.embedding_len)
         ))
+        self.linear_final = nn.Linear(config.embedding_len, config.vocab_size) # (B, T, vocab_size)
+    
+    # Recieves B sequences of T tokens. (B, T).
+    def forward(self, x):
+        '''
+        TODO: Add block for when in inference and we're only concerned with next token of last timestep.
+        '''
+        B, T= x.size()
+        assert T <= self.config.seq_len, f'sequence length should not exceed {self.config.seq_len}' # XXX: Should ==? otherwise mismatch with linear
+        token_embeddings = self.transformer['embedding_table'](x) # (B, T, C)
+        positions = torch.arange(0, T) # Should use actual T sequence length from input instead of config. Otherwise, boarding in addition fails.
+        pos_embeddings = self.transformer['positional_embedding'](positions) # (seq_len)
+        x = token_embeddings + pos_embeddings
+        for block in self.transformer['blocks']:
+            x = block(x)
+
+        x = self.transformer.ln(x)
+        logits = self.linear_final(x)
+        # If eval, we're only cconcerned with next token for last time step.
+        logits = logits if not self.training else logits[:,[-1], :] # [-1] brackets preserve dim. 
         
+        # Softmax over last dim (vocab_size) to get the probas for next token for each token in vocab
+        probas = F.softmax(logits, dim=-1)
+        vals, indices = probas.topk(self.top_picks, dim=-1)
+        
+
 '''
 Some notes:
 
 - GPT, differs than the transformer.png. It's a decoder-only transformer.
 the LN is done I think before the attention adn FW instead of after shown in the diagram.
-- attn and FW are each a residual block. their output to residual pathway.
+- attn and FW are each a residual block. their output is added to residual pathway.
 
 '''
 
+# =====
+#  ARGS
+batch_size = 4
+
+# =====
+
 conf = GPTConfig()
 print(conf.seq_len)
-s_attn = SelfAttention(conf)
-x = torch.rand(4, conf.seq_len, conf.embedding_len)
-out = s_attn(x)
-feed_fw = FeedForward(conf)
-out = feed_fw(out)
+# s_attn = SelfAttention(conf)
+# x = torch.rand(4, conf.seq_len, conf.embedding_len)
+# out = s_attn(x)
+# feed_fw = FeedForward(conf)
+# out = feed_fw(out)
 
-block = Block(conf)
-out = block(x)
+# block = Block(conf)
+# out = block(x)
+
+sequences = torch.randint(0, conf.vocab_size, (4, conf.seq_len - 3))
+gpt = GPT(conf)
+out = gpt(sequences)
